a33d48e98230d5d255dc1e7da47cb340a56437c2
Saarthak Slathia <77338782+Saarthak-Slathia@users.noreply.github.com>
2021-06-23 06:16:05 +0530

    Update main.java

--- main.java
+++ main.java
@@ -1,24 +1,30 @@
-
 class Event{
-  int date;
-  String name;
-  String month;
-  
-  void Event(int a, String b, String c){
-    a = date;
-    b = name;
-    c = month;
+    int date;
+    String name;
+    String month;
+    
+    void Event(int a, String b, String c){
+      a = date;
+      b = name;
+      c = month;
+    }
+
+    void setData(int x, String y, String z){
+        date = x;
+        name = y;
+        month = z;
+    }
+    
+    void displayData(){
+      System.out.println(name + " is celebrated on " + date + " " + month + ".");
+    }
+    
   }
-  
-  void displayData(){
-    System.out.println(name + " is celebrated on " + date + " " + month + ".");
-  }
-  
-}
 
 class main{
-  public static void main(String args[]){
-    Event yoga = new Event(21, "International Yoga Day", "June");
-    yoga.displayData();
-  }
+    public static void main(String args[]){
+        Event yoga = new Event();
+        yoga.setData(21, "International Yoga Day", "June");
+        yoga.displayData();
+    }
 }
